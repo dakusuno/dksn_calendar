34d5debec7d61b12cae8e92e223819a383c12864
Radit Ananta <37116978+dakusuno@users.noreply.github.com>
2025-09-08 11:32:52 +0800

    Update dksn_calendar.dart

--- lib/src/dksn_calendar.dart
+++ lib/src/dksn_calendar.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:dksn_calendar/src/dksn_calendar_controller.dart';
 import 'package:dksn_calendar/src/dksn_calendar_header.dart';
 import 'package:dksn_calendar/src/dksn_calendar_monthly_item.dart';
